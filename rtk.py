ef68379d40cb90ff63945edbd31fe2603e478a46
zyxkad <zyxkad@gmail.com>
2024-06-25 17:38:10 -0600

    reforged some backend logic

--- rtk.py
+++ rtk.py
@@ -1,59 +1,64 @@
-from flask import Flask, render_template, request, jsonify, session
-from pymavlink import mavutil
-from flask_caching import Cache
-from threading import Thread, Timer, Event
-from flask_socketio import SocketIO, emit
-import subprocess
-import logging
-import os
-import sys
-import csv
 from datetime import datetime
-import serial
+from threading import Thread, Timer, Event
+
+from flask import Flask, render_template, request, jsonify, session
+from flask_caching import Cache
+from flask_socketio import SocketIO, emit
+from pymavlink import mavutil
+
+import csv
+import logging
+import math
+import os
 import struct
-import serial.tools.list_ports
+import subprocess
+import sys
 import time
+
+import serial
+import serial.tools.list_ports
 
 app = Flask(__name__)
 app.secret_key = 'AUAVDRONE'
 os.environ['MAVLINK20'] = '1'
 flight_modes = {
-    0: "STABILIZE",
-    1: "ACRO",
-    2: "ALTHOLD",
-    3: "AUTO",
-    4: "GUIDED",
-    5: "LOITER",
-    6: "RTL",
-    7: "CIRCLE",
-    9: "LAND",
-    11: "DRIFT",
-    13: "SPORT",
-    14: "FLIP",
-    15: "AUTOTUNE",
-    16: "POSHOLD",
-    17: "BRAKE",
-    18: "THROW",
-    19: "AVOID_ADSB",
-    20: "GUIDED_NOGPS",
-    21: "SMART_RTL",
-    22: "FLOWHOLD",
-    23: "FOLLOW",
-    24: "ZIGZAG",
-    25: "SYSTEMID",
-    26: "HELI_AUTOROTATE",
-    27: "AUTO RTL"
+    0: 'STABILIZE',
+    1: 'ACRO',
+    2: 'ALTHOLD',
+    3: 'AUTO',
+    4: 'GUIDED',
+    5: 'LOITER',
+    6: 'RTL',
+    7: 'CIRCLE',
+    9: 'LAND',
+    11: 'DRIFT',
+    13: 'SPORT',
+    14: 'FLIP',
+    15: 'AUTOTUNE',
+    16: 'POSHOLD',
+    17: 'BRAKE',
+    18: 'THROW',
+    19: 'AVOID_ADSB',
+    20: 'GUIDED_NOGPS',
+    21: 'SMART_RTL',
+    22: 'FLOWHOLD',
+    23: 'FOLLOW',
+    24: 'ZIGZAG',
+    25: 'SYSTEMID',
+    26: 'HELI_AUTOROTATE',
+    27: 'AUTO RTL'
 }
 
 app.config['CACHE_TYPE'] = 'simple'
-cache = Cache(app)
+baud_rate = 115200
+drone_infos = {}
+drone_timeout_timers = {}
 socketio = SocketIO(app)
 global_connection = None
 global_rtk_connection = None
 motor_test_duration = 30
 stop_battery_logging = Event()
-rtk_status = {"satellites_visible": 0, "survey_in_progress": True, "rtk_precision": None}
-last_update_time = {}
+rtk_status = {'satellites_visible': 0, 'survey_in_progress': True, 'rtk_precision': None}
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.INFO)
 
@@ -66,86 +71,85 @@ class LogHandler(logging.Handler):
 log.addHandler(LogHandler())
 
 def setup_connection(com_port):
-    global global_connection
-    baud_rate = 115200
-    global_connection = mavutil.mavlink_connection(com_port, baud=baud_rate)
+    return mavutil.mavlink_connection(com_port, baud=baud_rate)
 
 def setup_rtk_connection(com_port, baud_rate):
-    global global_rtk_connection
-    global_rtk_connection = serial.Serial(com_port, baudrate=baud_rate, timeout=1)
+    return serial.Serial(com_port, baudrate=baud_rate, timeout=1)
     
-def listen_to_drones():
+def listen_to_drones(connection):
     while True:
-        if global_connection:
-            msg = global_connection.recv_match(type=['SYS_STATUS', 'GPS_RAW_INT', 'HEARTBEAT'], blocking=False)
-            if msg:
-                drone_id = msg.get_srcSystem()
-                update_drone_status(drone_id, msg)
+        msg = connection.recv_match(type=['SYS_STATUS', 'GPS_RAW_INT', 'HEARTBEAT'], blocking=False)
+        if not msg:
+            continue
+        drone_id = msg.get_srcSystem()
+        update_drone_status(drone_id, msg)
 
-def read_and_send_rtk_data():
+def read_and_send_rtk_data(connection, rtk_connection):
     sequence_id = 0
     msglen = 100
     while True:
-        data = global_rtk_connection.read(1024)
+        data = rtk_connection.read(1024)
         for i in range(0, len(data), msglen):
             chunk = data[i:i + msglen]
             start_time = time.time()
-            print(f"Received {len(chunk)}, GOOD!")
-            if (len(chunk) % msglen == 0):
-                msgs = len(chunk) // msglen
-            else:
-                msgs = (len(chunk) // msglen) + 1
-            for a in range(msgs):
+            print(f'Received {len(chunk)}, GOOD!')
+            msgs = math.ceil(len(chunk) / msglen)
+            for msg in range(msgs):
                 flags = 0
                 if msgs > 1:
                     flags = 1
-                flags |= (a & 0x3) << 1
-                flags |= (sequence_id & 0x1f) << 3
-                amount = min(len(chunk) - a * msglen, msglen)
-                datachunk = chunk[a * msglen : a * msglen + amount]
+                flags |= (msg & 0x3) << 1
+                flags |= sequence_id << 3
+                amount = min(len(chunk) - msg * msglen, msglen)
+                datachunk = chunk[msg * msglen : msg * msglen + amount]
                 
-                global_connection.mav.gps_rtcm_data_send(
+                connection.mav.gps_rtcm_data_send(
                     flags,
                     len(datachunk),
                     bytearray(datachunk.ljust(180, b'\0'))
                 )
             if msgs < 4 and len(chunk) % msglen == 0 and len(chunk) > msglen:
-                flags = 1 | (msgs & 0x3) << 1 | (sequence_id & 0x1f) << 3
-                global_connection.mav.gps_rtcm_data_send(
+                flags = 1 | (msgs & 0x3) << 1 | sequence_id << 3
+                connection.mav.gps_rtcm_data_send(
                     flags,
                     0,
-                    bytearray("".ljust(180, b'\0'))
+                    bytearray(''.ljust(180, b'\0'))
                 )
-            sequence_id += 1
+            sequence_id = (sequence_id + 1) & 0x1f
 
 def update_drone_status(drone_id, msg):
-    current_time = time.time()
-    last_update_time[drone_id] = current_time
-    drone_info = cache.get(f'drone_status_{drone_id}')
+    drone_timeout_timer = drone_timeout_timers.pop(drone_id, None)
+    if drone_timeout_timer:
+        drone_timeout_timer.stop()
+    drone_info = drone_infos.get(drone_id, None)
     if not drone_info:
         drone_info = {
-            "id": drone_id,
-            "mode": "--",
-            "voltage": "--",
-            "current": "--",
-            "gps_type": "--",
-            "gps_coords": ("--", "--")
+            'id': drone_id,
+            'status': 'ok',
+            'mode': '--',
+            'voltage': '--',
+            'current': '--',
+            'gps_type': '--',
+            'gps_coords': ('--', '--')
         }
     if msg.get_type() == 'HEARTBEAT':
-        drone_info["mode"] = flight_modes.get(msg.custom_mode, "Unknown")
+        drone_info['mode'] = flight_modes.get(msg.custom_mode, 'Unknown')
     elif msg.get_type() == 'SYS_STATUS':
-        drone_info["voltage"] = msg.voltage_battery / 1000.0
-        drone_info["current"] = msg.current_battery / 100.0
+        drone_info['voltage'] = msg.voltage_battery / 1000.0
+        drone_info['current'] = msg.current_battery / 100.0
     elif msg.get_type() == 'GPS_RAW_INT':
-        drone_info["gps_type"] = msg.fix_type
-        drone_info["gps_coords"] = (msg.lat / 1e7, msg.lon / 1e7)
+        drone_info['gps_type'] = msg.fix_type
+        drone_info['gps_coords'] = (msg.lat / 1e7, msg.lon / 1e7)
 
-    cache.set(f'drone_status_{drone_id}', drone_info)
-    Timer(3 , check_drone_timeout, [drone_id]).start()
+    socketio.emit('drone_info', drone_info)
+    drone_infos[drone_id] = drone_info
+    drone_timeout_timer = Timer(3 , mark_drone_timedout, (drone_id, drone_info))
+    drone_timeout_timer.start()
+    drone_timeout_timers[drone_id] = drone_timeout_timer
 
-def send_motor_test_command(drone_id, motor_instance, throttle):
+def send_motor_test_command(connection, drone_id, motor_instance, throttle):
     global motor_test_duration
-    global_connection.mav.command_long_send(
+    connection.mav.command_long_send(
         drone_id,
         1,
         mavutil.mavlink.MAV_CMD_DO_MOTOR_TEST,
@@ -159,25 +163,17 @@ def send_motor_test_command(drone_id, motor_instance, throttle):
         0
     )
 
-def check_drone_timeout(drone_id):
-    current_time = time.time()
-    if drone_id in last_update_time and (current_time - last_update_time[drone_id]) > 3:
-        drone_info = {
-            "id": drone_id,
-            "mode": "--",
-            "voltage": "--",
-            "current": "--",
-            "gps_type": "--",
-            "gps_coords": ("--", "--")
-        }
-        cache.set(f'drone_status_{drone_id}', drone_info)
+def mark_drone_timedout(drone_id, drone_info):
+    drone_info['status'] = 'timeout'
+    socketio.emit('drone_disconnected', drone_id, 'timeout')
 
 def send_led_control_message(drone_id, r, g, b, duration_msec, flashes):
     target_component = 1
     instance = 42
     pattern = 42
-    custom_len = 5
-    custom_bytes = [r, g, b, duration_msec % 256, duration_msec // 256] + [0]*19
+    custom_bytes = [r, g, b, duration_msec & 0xff, duration_msec >> 8]
+    custom_len = len(custom_bytes)
+    custom_bytes.extend([0] * (24 - custom_len))
     
     for _ in range(flashes):
         global_connection.mav.led_control_send(
@@ -185,15 +181,13 @@ def send_led_control_message(drone_id, r, g, b, duration_msec, flashes):
         )
         time.sleep(duration_msec / 1000 + 0.5)
 
-def get_drones():
-    drone_status_keys = cache.cache._cache.keys()
-    drones = [key.split('_')[-1] for key in drone_status_keys if key.startswith('drone_status_')]
-    return drones
+def get_active_drones():
+    return [drone_id for drone_id, drone_info in drone_infos.values() if drone_info['status'] == 'ok']
 
 @app.route('/list_ports', methods=['GET'])
 def list_ports():
     ports = list(serial.tools.list_ports.comports())
-    port_list = [{"device": port.device, "description": port.description} for port in ports]
+    port_list = [{'device': port.device, 'description': port.description} for port in ports]
     return jsonify(port_list)
 
 @app.route('/')
@@ -202,25 +196,27 @@ def index():
 
 @app.route('/set_com_port', methods=['POST'])
 def set_com_port():
+    global global_connection
     data = request.json
     com_port = data.get('com_port')
-    setup_connection(com_port)
-    thread = Thread(target=listen_to_drones)
+    global_connection = setup_connection(com_port)
+    thread = Thread(target=listen_to_drones, name='listen_to_drones', args=(global_connection,))
     thread.start()
-    socketio.emit('log_message', {'data': f"COM port {com_port} set"})
-    return jsonify(success=True, message="COM port set and listening started.")
+    socketio.emit('log_message', {'data': f'COM port {com_port} set'})
+    return jsonify(success=True, message='COM port set and listening started.')
 
 @app.route('/set_rtk_port', methods=['POST'])
 def set_rtk_port():
+    global global_rtk_connection
     data = request.json
     rtk_com_port = data.get('rtk_com_port')
     baudrate = int(data.get('baudrate'))
     if rtk_com_port:
-        setup_rtk_connection(rtk_com_port, baudrate)
-        rtk_thread = Thread(target=read_and_send_rtk_data)
+        global_rtk_connection = setup_rtk_connection(rtk_com_port, baudrate)
+        rtk_thread = Thread(target=read_and_send_rtk_data, name='read_and_send_rtk_data', args=(global_connection, global_rtk_connection,))
         rtk_thread.start()
-    socketio.emit('log_message', {'data': f"RTK port {rtk_com_port} set with baudrate {baudrate} and listening started."})
-    return jsonify(success=True, message="RTK port set and listening started.")
+    socketio.emit('log_message', {'data': f'RTK port {rtk_com_port} set with baudrate {baudrate} and listening started.'})
+    return jsonify(success=True, message='RTK port set and listening started.')
 
 @app.route('/motor_test', methods=['POST'])
 def motor_test():
@@ -228,61 +224,53 @@ def motor_test():
     drone_id = request.form.get('drone_id')
     throttle = float(request.form['throttle'])
 
-    if global_connection:
-        if drone_id == 'all':
-            socketio.emit('log_message', {'data': f"send to all drones start"})
-            drones = get_drones()
-            for d in drones:
-                d = int(d)
-                if motor_id == 'all':
-                    for motor_instance in range(1, 5):
-                        send_motor_test_command(d, motor_instance, throttle)
-                else:
-                    send_motor_test_command(d, int(motor_id), throttle)
-        else:
-            drone_id = int(drone_id)
-            if motor_id == 'all':
-                for motor_instance in range(1, 5):
-                    send_motor_test_command(drone_id, motor_instance, throttle)
-            else:
-                send_motor_test_command(drone_id, int(motor_id), throttle)
-
-    return jsonify(success=True, message="Motor test command sent.")
+    if not global_connection:
+        return jsonify(success=False, message='Not connected')
+    drones = []
+    motors = []
+    if drone_id == 'all':
+        socketio.emit('log_message', {'data': f'send to all drones start'})
+        drones = get_active_drones()
+    else:
+        drones = [int(drone_id)]
+    if motor_id == 'all':
+        motors = range(1, 5)
+    else:
+        motors = [int(motor_id)]
+    for d in drones:
+        for m in motors:
+            send_motor_test_command(global_connection, d, m, throttle)
+    return jsonify(success=True, message='Motor test command sent.')
 
 
 @app.route('/change_mode', methods=['POST'])
 def change_mode():
     mode_id = int(request.form['mode'])
     drone_id = request.form.get('drone_id')
-    if global_connection:
-        try:
-            if drone_id == 'all':
-                socketio.emit('log_message', {'data': f"send to all start"})
-                drones = get_drones()
-                for d in drones:
-                    d = int(d)
-                    global_connection.mav.set_mode_send(
-                        d,
-                        mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
-                        mode_id)
-                    socketio.emit('log_message', {'data': f"Mode change command received: DroneID: {drone_id}  ModeID: {mode_id}  Mode: {flight_modes.get(mode_id, 'Unknown')}"})
-            else:
-                drone_id = int(drone_id)
-                global_connection.mav.set_mode_send(
-                    drone_id,
-                    mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
-                    mode_id)
-                socketio.emit('log_message', {'data': f"Mode change command received: DroneID: {drone_id}  ModeID: {mode_id}  Mode: {flight_modes.get(mode_id, 'Unknown')}"})
-            return jsonify(success=True, message="Mode change command sent.")
-        except Exception as e:
-            return jsonify(success=False, message=str(e))
-    return jsonify(success=False, message="Connection not established.")
+    if not global_connection:
+        return jsonify(success=False, message='Connection not established.')
+    drones = []
+    if drone_id == 'all':
+        socketio.emit('log_message', {'data': f'send to all start'})
+        drones = get_active_drones()
+    else:
+        drones = [int(drone_id)]
+    try:
+        for d in drones:
+            global_connection.mav.set_mode_send(
+                d,
+                mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+                mode_id)
+            socketio.emit('log_message', {'data': f'Mode change command received: DroneID: {drone_id}  ModeID: {mode_id}  Mode: {flight_modes.get(mode_id, 'Unknown')}'})
+        return jsonify(success=True, message='Mode change command sent.')
+    except Exception as e:
+        return jsonify(success=False, message=str(e))
 
 @app.route('/change_color', methods=['POST'])
 def change_color():
     data = request.get_json()
     if not data:
-        return jsonify({"success": False, "message": "Invalid JSON data"})
+        return jsonify({'success': False, 'message': 'Invalid JSON data'})
     color = data['rgb']
     duration = data['duration']
     flashes = data['flashes']
@@ -292,26 +280,18 @@ def change_color():
     b = color['b']
     duration = int(duration)
     flashes = int(flashes)
+    drones = []
     if drone_id == 'all':
-        socketio.emit('log_message', {'data': f"send to all start"})
-        drones = get_drones()
-        for d in drones:
-            d = int(d)
-            send_led_control_message(d, r, g, b, duration, flashes)
-            socketio.emit('log_message', {'data': f"Color change command received: DroneID: {d}  RGB: ({r}, {g}, {b}), Duration: {duration} ms, Flashes: {flashes}"})
+        socketio.emit('log_message', {'data': f'send to all start'})
+        drones = get_active_drones()
     else:
-        socketio.emit('log_message', {'data': f"send to one start"})
-        drone_id = int(drone_id)
-        send_led_control_message(drone_id, r, g, b, duration, flashes)
-        socketio.emit('log_message', {'data': f"Color change command received: DroneID: {drone_id}  RGB: ({r}, {g}, {b}), Duration: {duration} ms, Flashes: {flashes}"})
+        socketio.emit('log_message', {'data': f'send to one start'})
+        drones = [int(drone_id)]
+    for d in drones:
+        send_led_control_message(d, r, g, b, duration, flashes)
+        socketio.emit('log_message', {'data': f'Color change command received: DroneID: {d}  RGB: ({r}, {g}, {b}), Duration: {duration} ms, Flashes: {flashes}'})
 
-    return jsonify(success=True, message="Color change command sent.")
-
-@app.route('/get_battery_status', methods=['GET'])
-def get_battery_status():
-    drone_ids = get_drones()
-    battery_statuses = {drone_id: cache.get(f'drone_status_{drone_id}') for drone_id in drone_ids}
-    return jsonify(success=True, battery_statuses=battery_statuses)
+    return jsonify(success=True, message='Color change command sent.')
 
 @app.route('/get_rtk_status', methods=['GET'])
 def get_rtk_status():
@@ -322,7 +302,7 @@ def handle_selected_block():
     data = request.get_json()
     drone_id = data['droneId']
     session['drone_id'] = drone_id
-    return jsonify(message=f"Received and saved drone ID: {drone_id}")
+    return jsonify(message=f'Received and saved drone ID: {drone_id}')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
