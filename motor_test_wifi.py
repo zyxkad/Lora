3010aec3623b96e656411ea1afa6fcac83231fa4
zyxkad <zyxkad@gmail.com>
2024-08-16 14:58:03 -0600

    add short interval runs

--- motor_test_wifi.py
+++ motor_test_wifi.py
@@ -24,6 +24,8 @@ def send_motor_test_command(connection, motor_id, throttle, duration):
         0,
         0
     )
+    ack = connection.recv_match(type='COMMAND_ACK', blocking=True)
+    # print('command ack:', ack)
     # print(f"Sent motor test command to motor {motor_id}")
 
 def listen_and_log_battery_info(connection, filename):
@@ -85,12 +87,27 @@ def split_and_visualize_battery_data(base_folder, filename):
         drone_data.to_csv(drone_filename, index=False)
         visualize_battery_data(drone_data, drone_folder, drone_id, timestamp)
 
+turn_off_timer = None
+
+def turn_off_led():
+    dur = 0xffff
+    bts = [0, 0, 0, dur & 0xff, (dur >> 8) & 0xff]
+    btslen = len(bts)
+    bts.extend([0] * (24 - btslen))
+    connection.mav.led_control_send(
+        0, 1, 42, 42, btslen, bts,
+    )
+    global turn_off_timer
+    turn_off_timer = threading.Timer(60, turn_off_led)
+    turn_off_timer.start()
+
 def main():
     global connection
     current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
     base_folder = current_time
 
-    connection = mavutil.mavlink_connection('192.168.0.251:14550')
+    connection = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
+    print("Waiting heartbeat...")
     connection.wait_heartbeat()
 
     drone_ids = set()
@@ -100,7 +117,9 @@ def main():
         msg = connection.recv_match(type='SYS_STATUS', blocking=False)
         if msg:
             drone_id = msg.get_srcSystem()
-            drone_ids.add(drone_id)
+            if drone_id not in drone_ids:
+                print('found', drone_id)
+                drone_ids.add(drone_id)
         time.sleep(0.1)
 
     user_input = input(f"Do you want to proceed with testing drones with IDs {list(drone_ids)}? (y/n): ")
@@ -112,33 +131,55 @@ def main():
         os.makedirs(base_folder)
     filename = os.path.join(base_folder, f'battery_info_{current_time}.csv')
 
+    turn_off_led()
+    time.sleep(0.1)
+
     print("Starting battery logging...")
     battery_logging_thread = threading.Thread(target=listen_and_log_battery_info, args=(connection, filename))
     battery_logging_thread.start()
     time.sleep(5)
 
+    # Long run test
     throttle = 60
     duration = 30
-    for i in range(2):
-        for motor_id in range(1, 5):
-            send_motor_test_command(connection, motor_id, throttle, duration)
-            time.sleep(0.05)
-
+    for motor_id in range(1, 5):
+        send_motor_test_command(connection, motor_id, throttle, duration)
     time.sleep(duration + 5)
+
+    # Motor start up test
+    short_run_round = 3
+    short_run_duration = 5
+    for i in range(short_run_round):
+        for motor_id in range(1, 5):
+            send_motor_test_command(connection, motor_id, throttle, short_run_duration)
+        time.sleep(short_run_duration + 5)
+
+    # Motor acceleration test
+    accel_switch_round = 3
+    accel_switch_interval = 5
+    accel_switch_speeds = [60, 30]
+    for i in range(accel_switch_round):
+        for speed in accel_switch_speeds:
+            for motor_id in range(1, 5):
+                send_motor_test_command(connection, motor_id, speed, accel_switch_interval + 1)
+            time.sleep(accel_switch_interval)
+
+    time.sleep(5)
 
     stop_battery_logging.set()
     battery_logging_thread.join()
     split_and_visualize_battery_data(base_folder, filename)
     print("Test completed.")
+    turn_off_timer.cancel()
 
 def stop_all_testing_motors():
-    for i in range(2):
-        for motor_id in range(1, 5):
-            send_motor_test_command(connection, motor_id, 0, 0)
-            time.sleep(0.05)
+    for motor_id in range(1, 5):
+        send_motor_test_command(connection, motor_id, 0, 0)
 
 if __name__ == '__main__':
     try:
         main()
+    except KeyboardInterrupt:
+        pass
     finally:
         stop_all_testing_motors()
